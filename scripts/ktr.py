d8bd88d8e005995673c21afc8e81d6540eb42ea6
NetDenizen <NetDenizen@users.noreply.github.com>
2018-12-12 23:35:36 -0500

    Update ktr.py

--- scripts/ktr.py
+++ scripts/ktr.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
+
 import sys
 from kanji_to_romaji.kanji_to_romaji_module import kanji_to_romaji
 
 if __name__ == "__main__":
-    print(kanji_to_romaji(("".join(sys.argv[1:])).decode('unicode-escape')))
+    print(kanji_to_romaji(("".join(sys.argv[1:]))))
